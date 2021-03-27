c0f8e58b506cb8e6220c652a2b73d103f4ab54e6
liweishuang <liweishuang2020@163.com>
2021-03-27 14:48:35 +0800

    CountDownLatchDemo 修改，添加枚举类

--- src/jucDemo/CountDownLatchDemo.java
+++ src/jucDemo/CountDownLatchDemo.java
@@ -10,7 +10,8 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchDemo {
 
 	public static void main(String[] args) throws InterruptedException {
-		general();
+		//System.out.println(CountryEnum.forEach_CountryEnum(2).getRetMessage());
+		countDownLatchTest();
 	}
 	
 	public static void general() throws InterruptedException {
@@ -26,13 +27,15 @@ public class CountDownLatchDemo {
 	}
 	
 	
-	public static void countDownLatchTest() {
+	public static void countDownLatchTest() throws InterruptedException {
 		CountDownLatch countDownLatch = new CountDownLatch(6);
-		for(int i=0;i<6;i++) {
+		for(int i=1;i<=6;i++) {
 			new Thread(()->{
-				System.out.println();
-			}).start();
+				System.out.println(Thread.currentThread().getName()+"\t ±»Ãð£¡");
+				countDownLatch.countDown();
+			},CountryEnum.forEach_CountryEnum(i).getRetMessage()).start();
 		}
-		
+		countDownLatch.await();
+		System.out.println(Thread.currentThread().getName()+"->ÇØ³¯Í³Ò»£¡");
 	}
 }
