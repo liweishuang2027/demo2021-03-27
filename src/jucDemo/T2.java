61e244adbff8532a415262fdecabec48a6af5e57
liweishuang <liweishuang2020@163.com>
2021-04-04 13:41:38 +0800

    T2

--- src/jucDemo/T2.java
+++ src/jucDemo/T2.java
@@ -2,9 +2,10 @@ package jucDemo;
 
 public class T2 {
 	public static void main(String[] args) {
-		Thread t1 = new Thread();
-		t1.start();
-		t1.start();
+		
+		System.out.println(Runtime.getRuntime().availableProcessors());
+		
+		
 	}
 
 }
