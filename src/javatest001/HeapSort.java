e2ac74ab8d9ec7201e29eca6fce56a5a1bbd82c6
liweishuang <liweishuang2020@163.com>
2021-05-29 14:55:15 +0800

    堆排序算法

---
+++ src/javatest001/HeapSort.java
@@ -0,0 +1,26 @@
+package javatest001;
+/**
+ * ¶ÑÅÅĞò
+ * @author lws
+ *
+ */
+public class HeapSort {
+	public static void main(String[] args) {
+		int[] arr = {9,8,7,6,5,4,3,2,1};
+		
+	}
+	
+	public static void sort(int[] arr) {
+		
+	}
+
+	/**
+	 * µ÷Õû´ó¶¥¶Ñ
+	 * @param arr
+	 * @param i
+	 * @param length
+	 */
+	public static void adjustHeap(int[] arr,int i,int length) {
+		
+	}
+}
